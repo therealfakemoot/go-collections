b03642d081113249e95e3c8dd17c7daa5213797d
Nick Dumas <dumasn@homes.com>
2020-02-26 10:56:30 -0500

    Refactoring Start() to be testable

--- lmru/lmru.go
+++ lmru/lmru.go
@@ -1,14 +1,15 @@
 package main
 
-var lmruTemplate = `
+var lmruTemplate = `package {{ .Package }}
+
 import (
 	"fmt"
 	"sync"
 	"time"
 )
 
-{{ if .L }}// LRU is a map that evicts items based on their last time of use.{{ else }}// MRU is a map that evicts items based on their last time of use.{{ end }}
-type {{if .L}}L{{else}}M{{end}}{{.Name}} struct {
+{{ if .L }}// {{ .Name }} is a map that evicts items based on their last time of use.{{ else }}// {{ .Name }} is a map that evicts items based on their last time of use.{{ end }}
+type {{ .Name }} struct {
 	sync.Mutex
 	timeout time.Duration
 	m       map[string]{{ .Type }}      // this is the payload data
@@ -16,14 +17,14 @@ type {{if .L}}L{{else}}M{{end}}{{.Name}} struct {
 }
 
 // Get returns the value associated with k
-func (m *MRU{{.Name}}) Get(k string) {{ .Type }} {
+func (m *{{ .Name }}) Get(k string) {{ .Type }} {
 	m.Lock()
 	defer m.Unlock()
 	return m.m[k]
 }
 
 // Set assigns the value to k and stores the creation time of this entry.
-func (m *MRU{{.Name}}) Set(k string, v {{ .Type }}) {
+func (m *{{ .Name }}) Set(k string, v {{ .Type }}) {
 	m.Lock()
 	defer m.Unlock()
 	m.m[k] = v
@@ -31,17 +32,17 @@ func (m *MRU{{.Name}}) Set(k string, v {{ .Type }}) {
 }
 
 // Start begins the goroutine that cleans up entries from the LMRU.
-func (m *MRU{{.Name}}) Start(d time.Duration) {
+func (m *{{ .Name }}) Start(c chan time.Time) {
 	go func() {
-		for range time.NewTicker(d).C {
+		for t := range c {
 			m.Lock()
-			defer m.Unlock()
 			for k := range m.m {
-				if time.Now().Sub(m.c[k]){{if .L}} > {{ else }} < {{ end }} m.timeout {
+				if t.Sub(m.c[k]){{if .L}} > {{ else }} < {{ end }}m.timeout {
 					delete(m.m, k)
 				}
 
 			}
+			m.Unlock()
 		}
 	}()
 }
--- lmru/main.go
+++ lmru/main.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"flag"
+	"log"
 	"os"
 	"strings"
 	"text/template"
 )
 
 type data struct {
-	Type string
-	Name string
-	L    bool // whether this is an mru or lru
+	Type    string
+	Name    string
+	L       bool // whether this is an mru or lru
+	Package string
 }
 
 func main() {
 	var d data
 	flag.StringVar(&d.Type, "valueType", "bool", "valueType to store in cache")
+	flag.StringVar(&d.Package, "pacakge", "main", "package name")
 	flag.BoolVar(&d.L, "lru", false, "least recently used?")
 
 	flag.Parse()
 
 	d.Name = strings.Title(d.Type)
 
+	d.Name = "MRU" + d.Name
+	if d.L {
+		d.Name = "LRU" + d.Name
+	}
+
 	t := template.Must(template.New("lmru").Parse(lmruTemplate))
-	t.Execute(os.Stdout, d)
+	err := t.Execute(os.Stdout, d)
+	if err != nil {
+		log.Fatalf("error generating type: %s", err)
+	}
 }
